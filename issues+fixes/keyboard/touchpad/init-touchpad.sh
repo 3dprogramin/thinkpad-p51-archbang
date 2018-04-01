894e879a88615358f77f2415b5351e4aed8db2cc
3dprogramin <admin@3dprogramin.com>
2018-04-01 12:56:34 +0300

    added tap speed (faster)

--- issues+fixes/keyboard/touchpad/init-touchpad.sh
+++ issues+fixes/keyboard/touchpad/init-touchpad.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/bash
 
 # enable tapping
-xinput set-prop "SynPS/2 Synaptics TouchPad" "libinput Tapping Enabled" 1
+#xinput set-prop "SynPS/2 Synaptics TouchPad" "libinput Tapping Enabled" 1
 # enable scrolling as on windows (natural scrolling config)
-xinput set-prop "SynPS/2 Synaptics TouchPad" "libinput Natural Scrolling Enabled" 1
+#xinput set-prop "SynPS/2 Synaptics TouchPad" "libinput Natural Scrolling Enabled" 1
 # set accelaration to .5
-xinput set-prop "SynPS/2 Synaptics TouchPad" "libinput Accel Speed" .7
+xinput set-prop "SynPS/2 Synaptics TouchPad" 268 1.7
+
+# set tap speed
+xinput set-prop 17 317 5000
 
 # set trackpoint acceleration to .3
-xinput set-prop 'TPPS/2 IBM TrackPoint' 'libinput Accel Speed' .3
+#xinput set-prop 'TPPS/2 IBM TrackPoint' 'libinput Accel Speed' .3
