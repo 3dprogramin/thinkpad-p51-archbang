bc2dac858d1597302795e0f9b521c28de25d4668
3dprogramin <andrei@3dprogramin.io>
2020-10-25 11:42:45 +0200

    tilda fix for multihead

--- issues+fixes/power/dock/multihead/multihead.sh
+++ issues+fixes/power/dock/multihead/multihead.sh
@@ -3,8 +3,16 @@
 # notify user what's about to happen
 notify-send Multihead "Switching to external monitors..."
 
-# kill tildas
-(killall tilda && sleep 1 && tilda -c "htop" -x 1) &
+# TILDA
+# --------------------------------------------------------------------------
+killall tilda		# kill current tildas
+tilda -c "htop" -x 1 &	# start tilda on monitor 1
+# start 2nd tilda (we do it like this, because we want to control it with F3 not F2, this is a 'dummy' instance, will be killed later on)
+# F2 is useful as it is, in renaming files / variables
+sleep 1 && tilda &
+# get PID of dummy instance
+sleep 2 && TILDA_PID=`ps -ef --sort=start_time | grep tilda | tail -n 2 | head -n 1 | awk '{print $2}'`
+# --------------------------------------------------------------------------
 
 # kill redshift sessions
 killall redshift
@@ -17,13 +25,16 @@ bash /home/icebox/Documents/scripts/multihead/xrandr_1.sh
 bash /home/icebox/Documents/scripts/multihead/xrandr_2.sh
 
 # conky for 2nd monitor
-(sleep 3 && conky -c ~/.config/conky/conky-monitor-2.conf) &
+conky -c ~/.config/conky/conky-monitor-2.conf &
 
 # redshift for entire display
 (sleep 8 && DISPLAY=:8 nohup redshift) &
-# remove redshift nohup file
+# remove nohup file
 (sleep 10 && rm nohup.out) &
+
 # set 0 backlight to laptop display
-(sleep 3 && notify-send Multihead "Ready" && sleep 2 && xbacklight -set 0) &
-# tilda
+(sleep 4 && notify-send Multihead "Switched monitors" && sleep 2 && xbacklight -set 0) &
+
+# tilda, start 3nd one (with F3) and kill 2nd one started above
 (sleep 10 && tilda -h -c "journalctl -f" -x 1920 -y 0) &
+(sleep 12 && kill -9 $TILDA_PID && notify-send Multihead Ready) &
